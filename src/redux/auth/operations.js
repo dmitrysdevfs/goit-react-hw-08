229fdedf237aea9c56c473edd062eb7d1dafd8f3
Dmytro <d.salyakin@gmail.com>
2025-04-13 18:10:52 +0300

    Implement setAuthHeader to manage Authorization token in API requests

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+const setAuthHeader = value => {
+  axios.defaults.headers.common.Authorization = value;
+};
+
 export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
     try {
       const response = await axios.post('/users/signup', credentials);
+      setAuthHeader(`Bearer ${response.data.token}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -18,6 +23,8 @@ export const logIn = createAsyncThunk(
   async (credentials, thunkAPI) => {
     try {
       const response = await axios.post('/users/login', credentials);
+      setAuthHeader(`Bearer ${response.data.token}`);
+      console.log(response.data.token);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
